8132ed475a1dd3ae21e3cb86340c8398809e4c58
Saurabh Tarar <saurabhtarar1998@gmail.com>
2025-03-23 16:40:37 +0530

    adding project to repo

---
+++ src/main/java/GrowNet/MavenHelloWorldTest/Test.java
@@ -0,0 +1,13 @@
+package GrowNet.MavenHelloWorldTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class Test 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Maven Hello World Test !" );
+    }
+}
